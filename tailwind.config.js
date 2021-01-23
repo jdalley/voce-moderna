7e6e5b51499ce9f5c453877fccc3a7ce62e50b9a
Jeff Dalley <jeffdalley@gmail.com>
2021-01-23 00:35:12 -0600

    Starting over with a basic Next.js setup with Tailwind and Typescript

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
