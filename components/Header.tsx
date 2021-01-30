ab2bd3d230d52a6d44fe714f5ad493c2bc1ab7eb
Jeff Dalley <jeffdalley@gmail.com>
2021-01-29 22:32:54 -0600

    Initial TailwindUI component testing

---
+++ components/Header.tsx
@@ -0,0 +1,231 @@
+import { Transition } from '@headlessui/react';
+import { useState } from 'react';
+
+const links = [
+  { href: 'https://github.com/vercel/next.js', label: 'GitHub' },
+  { href: 'https://nextjs.org/docs', label: 'Docs' }
+];
+
+export default function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  return (
+    <div className="relative bg-white">
+      <div className="flex justify-between items-center px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
+        <div className="flex justify-start lg:w-0 lg:flex-1">
+          <a href="#">
+            <span className="sr-only">Workflow</span>
+            <img
+              className="h-8 w-auto sm:h-10"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt=""
+            ></img>
+          </a>
+        </div>
+        <div className="-mr-2 -my-2 md:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+            className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+          >
+            <span className="sr-only">Open menu</span>
+            {/* Heroicon name: menu */}
+            <svg
+              className="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+        <nav className="hidden md:flex space-x-10">
+          <a
+            href="#"
+            className="text-base font-medium text-gray-500 hover:text-gray-900"
+          >
+            Database
+          </a>
+          <a
+            href="#"
+            className="text-base font-medium text-gray-500 hover:text-gray-900"
+          >
+            About VM
+          </a>
+          <a
+            href="#"
+            className="text-base font-medium text-gray-500 hover:text-gray-900"
+          >
+            Contact
+          </a>
+        </nav>
+        <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+          <a
+            href="#"
+            className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+          >
+            Cart
+          </a>
+        </div>
+      </div>
+
+      {/*	Mobile menu, show/hide based on mobile menu state. */}
+      <Transition
+        show={mobileMenuOpen}
+        enter="duration-200 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        {(ref) => (
+          <div
+            ref={ref}
+            className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+          >
+            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+              <div className="pt-5 pb-6 px-5">
+                <div className="flex items-center justify-between">
+                  <div>
+                    <img
+                      className="h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                      alt="Workflow"
+                    ></img>
+                  </div>
+                  <div className="-mr-2">
+                    <button
+                      type="button"
+                      onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+                      className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                    >
+                      <span className="sr-only">Close menu</span>
+                      {/* Heroicon name: x */}
+                      <svg
+                        className="h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <div className="mt-6">
+                  <nav className="grid grid-cols-1 gap-7">
+                    <a
+                      href="#"
+                      className="-m-3 p-3 flex items-center rounded-lg hover:bg-gray-50"
+                    >
+                      <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white">
+                        {/* Heroicon name: chart-bar */}
+                        <svg
+                          className="h-6 w-6"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                          />
+                        </svg>
+                      </div>
+                      <div className="ml-4 text-base font-medium text-gray-900">
+                        Database
+                      </div>
+                    </a>
+
+                    <a
+                      href="#"
+                      className="-m-3 p-3 flex items-center rounded-lg hover:bg-gray-50"
+                    >
+                      <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white">
+                        {/* Heroicon name: cursor-click */}
+                        <svg
+                          className="h-6 w-6"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
+                          />
+                        </svg>
+                      </div>
+                      <div className="ml-4 text-base font-medium text-gray-900">
+                        About VM
+                      </div>
+                    </a>
+
+                    <a
+                      href="#"
+                      className="-m-3 p-3 flex items-center rounded-lg hover:bg-gray-50"
+                    >
+                      <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white">
+                        {/* Heroicon name: shield-check */}
+                        <svg
+                          className="h-6 w-6"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+                          />
+                        </svg>
+                      </div>
+                      <div className="ml-4 text-base font-medium text-gray-900">
+                        Contact
+                      </div>
+                    </a>
+                  </nav>
+                </div>
+              </div>
+              <div className="py-6 px-5">
+                <div className="mt-6">
+                  <a
+                    href="#"
+                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                  >
+                    Cart
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import Nav from '../components/nav'
+import Header from '../components/Header';
 
 export default function Index() {
   return (
     <div>
-      <Nav />
+      <Header />
       <div className="py-20">
         <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-          Next.js + Tailwind CSS 2.0
+          Project Voce Moderna
         </h1>
       </div>
     </div>
-  )
+  );
 }
