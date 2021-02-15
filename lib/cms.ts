65343f3b0eafb64a64c51505e6b2c3171f94838d
Jeff Dalley <jeffdalley@gmail.com>
2021-02-14 23:09:35 -0600

    Last commit pre-move to Sanity

--- lib/cms.ts
+++ lib/cms.ts
@@ -1,133 +1,22 @@
-import Prismic from 'prismic-javascript';
+import Prismic from '@prismicio/client';
 
 const repository = process.env.PRISMIC_REPOSITORY_NAME;
 const clientAPIUrl = `https://${repository}.prismic.io/api/v2`;
-const graphqlAPIUrl = `https://${repository}.prismic.io/graphql`;
-
 const APIToken = process.env.PRISMIC_API_TOKEN;
 const APILocale = process.env.PRISMIC_REPOSITORY_LOCALE;
 
-export const prismicClient = Prismic.client(clientAPIUrl, {
-  accessToken: APIToken
-});
-
-// Type me later?
-export type PreviewData = any;
-export type QueryProps = {
-  previewData?: PreviewData;
-  variables?: any;
-};
-
-export default async function runQuery(
-  query: string,
-  { previewData, variables }: QueryProps = {}
-) {
-  const prismicAPI = await prismicClient.getApi();
-  const res = await fetch(
-    `${graphqlAPIUrl}?query=${query}&variables=${JSON.stringify(variables)}`,
-    {
-      headers: {
-        'Prismic-Ref': previewData?.ref || prismicAPI.masterRef.ref,
-        'Content-Type': 'application/json',
-        'Accept-Language': APILocale,
-        Authorization: `Token ${APIToken}`
-      }
-    }
-  );
-
-  if (res.status !== 200) {
-    console.log(await res.text());
-    throw new Error('Failed to fetch API');
-  }
-
-  const json = await res.json();
-  if (json.errors) {
-    console.error(json.errors);
-    throw new Error('Failed to fetch API');
-  }
-  return json.data;
+// Client method to query documents from the Prismic repo
+export default function PrismicClient(req = null) {
+  return Prismic.client(clientAPIUrl, createClientOptions(req, APIToken));
 }
 
-export async function getAllArias(previewData: PreviewData) {
-  const data = await runQuery(
-    `
-		{
-			allArias {
-				edges {
-					node {
-						_meta {
-							uid
-						}
-						title
-						description
-						character
-						voice_type {
-							... on Voicetype {
-								name
-							}
-						}
-						score_link {
-							publisher
-							link {
-								... on _ExternalLink {
-									url
-								}
-							}
-						}
-						tessitura
-						range
-						performances {
-							link_description
-							link {
-								... on _ExternalLink {
-									url
-								}
-							}
-						}
-						piano_track {
-							track_id
-							track_name
-							track_price
-							track_url
-							track_description
-							file_guid
-						}
-						vocal_track {
-							track_id
-							track_name
-							track_price
-							track_url
-							track_description
-							file_guid
-						}
-						opera {
-							... on Opera {
-								title
-								
-								librettists {
-									librettist {
-										... on Creator {
-											first_name
-											last_name
-										}
-									}
-								}
-								composers {
-									composer {
-										... on Creator {
-											first_name
-											last_name
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	`,
-    { previewData }
-  );
-  return data?.allArias?.edges;
+function createClientOptions(req = null, prismicAccessToken = null) {
+  const reqOption = req ? { req } : {};
+  const accessTokenOption = prismicAccessToken
+    ? { accessToken: prismicAccessToken }
+    : {};
+  return {
+    ...reqOption,
+    ...accessTokenOption
+  };
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,64 +1,83 @@
 import Layout, { LayoutProps } from '../components/Layout';
-import { getAllArias } from '../lib/cms';
+import PrismicClient from '../lib/cms';
+import Prismic from '@prismicio/client';
 
-export default function Index(props) {
+export default function Index(props: any) {
   const layoutProps: LayoutProps = {
     customMeta: {
       title: 'Home'
     }
   };
 
-  const aria = props.allArias.length > 0 ? props.allArias[0].node : null;
+  console.log({ ...props.arias });
+
+  const arias = props.arias.results;
 
   return (
     <Layout customMeta={layoutProps.customMeta}>
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-          Project Voce Moderna
-        </h1>
-
-        <div className="mt-28 text-center">
-          {aria.title[0].text} - ({aria.voice_type.name})
-        </div>
-        <div className="mb-10 text-center">{aria.opera.title[0].text}</div>
-        <div className="text-center">
-          <button
-            className="snipcart-add-item inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            data-item-id={aria.piano_track[0].track_id}
-            data-item-price={aria.piano_track[0].track_price}
-            data-item-url="/"
-            data-item-description={
-              aria.piano_track[0].track_description[0].text
-            }
-            data-item-name={aria.piano_track[0].track_name}
-            data-item-file-guid={aria.vocal_track[0].file_guid}
-          >
-            Buy Piano Track
-          </button>
-          <button
-            className="snipcart-add-item mx-4 inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            data-item-id={aria.vocal_track[0].track_id}
-            data-item-price={aria.vocal_track[0].track_price}
-            data-item-url="/"
-            data-item-description={
-              aria.vocal_track[0].track_description[0].text
-            }
-            data-item-name={aria.vocal_track[0].track_name}
-            data-item-file-guid={aria.vocal_track[0].file_guid}
-          >
-            Buy Vocal Track
-          </button>
-        </div>
-      </div>
+      <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
+        Project Voce Moderna
+      </h1>
+      <h2 className="text-5xl text-center text-gray-700 dark:text-gray-100">
+        Listing of {arias.length} arias
+      </h2>
+      {arias.map((aria) => {
+        return (
+          <div key={aria.uid} className="py-5">
+            <div className="text-center">
+              {aria.uid} - {aria.data.title[0].text}
+              {/* {aria.title[0].text} - ({aria.voice_type.name}) */}
+            </div>
+            {/* <div className="text-center">
+              <button
+                className="snipcart-add-item inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                data-item-id={aria.piano_track[0].track_id}
+                data-item-price={aria.piano_track[0].track_price}
+                data-item-url="/"
+                data-item-description={
+                  aria.piano_track[0].track_description[0].text
+                }
+                data-item-name={aria.piano_track[0].track_name}
+                data-item-file-guid={aria.vocal_track[0].file_guid}
+              >
+                Buy Piano Track
+              </button>
+              <button
+                className="snipcart-add-item mx-4 inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                data-item-id={aria.vocal_track[0].track_id}
+                data-item-price={aria.vocal_track[0].track_price}
+                data-item-url="/"
+                data-item-description={
+                  aria.vocal_track[0].track_description[0].text
+                }
+                data-item-name={aria.vocal_track[0].track_name}
+                data-item-file-guid={aria.vocal_track[0].file_guid}
+              >
+                Buy Vocal Track
+              </button>
+            </div> */}
+          </div>
+        );
+      })}
     </Layout>
   );
 }
 
 export async function getStaticProps({ preview = false, previewData }) {
-  const allArias = await getAllArias(previewData);
+  const client = PrismicClient();
 
-  console.log('index getStaticProps ran');
+  const arias = await client.query(
+    [Prismic.Predicates.at('document.type', 'aria')],
+    {
+      pageSize: 10,
+      orderings: '[document.first_publication_date]',
+      fetchLinks: 'opera.title'
+    }
+  );
+
+  console.log('index -> getStaticProps was run.');
+
   return {
-    props: { preview, allArias }
+    props: { preview, arias }
   };
 }
