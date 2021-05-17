7e25307c6947f3c293f1ef7a5985f7e15678dd1a
Jeff Dalley <jeffdalley@gmail.com>
2021-05-16 20:18:22 -0500

    Progress bar added to router

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,5 +1,12 @@
 import { AppProps } from 'next/app';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import '@styles/index.css';
+import '@styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
