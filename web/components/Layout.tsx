121f01369b630defb9b0224d878454ac08915300
Jeff Dalley <jeffdalley@gmail.com>
2021-12-03 08:55:58 -0500

    Added meta keywords

--- web/components/Layout.tsx
+++ web/components/Layout.tsx
@@ -18,7 +18,10 @@ export default function Layout({ children, customMeta, preview }: LayoutProps) {
   const router = useRouter();
   const meta = {
     title: 'Project Voce Moderna',
-    description: 'Voce Moderna is a database of contemporary opera arias, including information about composers and librettists, individual arias, full opera synopses, score links, and practice tracks for download.',
+    description:
+      'Voce Moderna is a database of contemporary opera arias, including information about composers and librettists, individual arias, full opera synopses, score links, and practice tracks for download.',
+    keywords:
+      'opera, operas, aria, arias, opera database, aria database, contemporary opera, contemporary music, new opera, new music, synopses, practice tracks, piano tracks, accompaniment tracks',
     image: 'https://www.projectvocemoderna.com/images/vm-logo-colour.png',
     type: 'website',
     ...customMeta,
@@ -29,7 +32,8 @@ export default function Layout({ children, customMeta, preview }: LayoutProps) {
       <Head>
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
-        <meta content={meta.description} name="description" />
+        <meta name="description" content={meta.description} />
+        <meta name="keywords" content={meta.keywords} />
         <meta
           property="og:url"
           content={`https://www.projectvocemoderna.com${router.asPath}`}
