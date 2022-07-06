4da0bb8e2228e6a210074f77e1e327034a199861
Jeff Dalley <jeffdalley@gmail.com>
2022-07-06 00:31:50 -0400

    Fixing missing track buttons, add track request text and mailto link

--- web/components/TrackButtons.tsx
+++ web/components/TrackButtons.tsx
@@ -17,42 +17,67 @@ export default function TrackButtons({
     'sm:inline-block sm:w-auto'
   );
 
-  return (
-    <>
-      <button
-        className={classNames(
-          buttonClasses,
-          'from-red-600 to-rose-500',
-          'hover:from-red-700 hover:to-rose-600'
+  if (
+    (pianoTrack === undefined || !pianoTrack) &&
+    (vocalTrack === undefined || !vocalTrack)
+  ) {
+    return (
+      <>
+        <div className="h-full text-right text-gray-900">
+          <p>Want to request a track? Email us here:</p>
+          <div>
+            <a
+              className="ml-1 text-cyan-600 hover:underline"
+              href="mailto:info@projectvocemoderna.com"
+            >
+              info@projectvocemoderna.com
+            </a>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        {pianoTrack !== undefined && pianoTrack && (
+          <button
+            className={classNames(
+              buttonClasses,
+              'from-red-600 to-rose-500',
+              'hover:from-red-700 hover:to-rose-600'
+            )}
+            data-item-id={pianoTrack.id}
+            data-item-price={pianoTrack.price}
+            data-item-url={pianoTrack.relativeUrl}
+            data-item-description={pianoTrack.description}
+            data-item-name={pianoTrack.name}
+            data-item-file-guid={pianoTrack.fileGuid}
+            data-item-max-quantity="1"
+          >
+            Buy Piano Track
+          </button>
         )}
-        data-item-id={pianoTrack.id}
-        data-item-price={pianoTrack.price}
-        data-item-url={pianoTrack.relativeUrl}
-        data-item-description={pianoTrack.description}
-        data-item-name={pianoTrack.name}
-        data-item-file-guid={pianoTrack.fileGuid}
-        data-item-max-quantity="1"
-      >
-        Buy Piano Track
-      </button>
-      <button
-        className={classNames(
-          buttonClasses,
-          'mt-2',
-          'sm:my-0 sm:ml-4',
-          'from-blue-600 to-violet-500',
-          'hover:from-blue-700 hover:to-violet-600'
+        {vocalTrack !== undefined && vocalTrack && (
+          <button
+            className={classNames(
+              buttonClasses,
+              'mt-2',
+              'sm:my-0 sm:ml-4',
+              'from-blue-600 to-violet-500',
+              'hover:from-blue-700 hover:to-violet-600'
+            )}
+            data-item-id={vocalTrack.id}
+            data-item-price={vocalTrack.price}
+            data-item-url={vocalTrack.relativeUrl}
+            data-item-description={vocalTrack.description}
+            data-item-name={vocalTrack.name}
+            data-item-file-guid={vocalTrack.fileGuid}
+            data-item-max-quantity="1"
+          >
+            Buy Vocal Track
+          </button>
         )}
-        data-item-id={vocalTrack.id}
-        data-item-price={vocalTrack.price}
-        data-item-url={vocalTrack.relativeUrl}
-        data-item-description={vocalTrack.description}
-        data-item-name={vocalTrack.name}
-        data-item-file-guid={vocalTrack.fileGuid}
-        data-item-max-quantity="1"
-      >
-        Buy Vocal Track
-      </button>
-    </>
-  );
+      </>
+    );
+  }
 }
